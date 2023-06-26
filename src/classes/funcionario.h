892b1573f2dd6f7188b69a7b8261cd0ea82d6599
Caio Lucas <lienscarlet@gmail.com>
2023-06-26 15:27:08 -0300

    feat: classe funcionario adicionada

---
+++ src/classes/funcionario.h
@@ -0,0 +1,45 @@
+  #pragma once
+
+  #include <string>
+  #include "pessoa.h"
+
+  using namespace std;
+
+  class Funcionario : public Pessoa
+  {
+  private:
+    string nome_usuario;
+    string senha;
+    string tipo;
+    string funcao;
+    double salario_hora;
+
+  public:
+    Funcionario(string nome_usuario = "", string senha = "", string tipo = "", string funcao = "", double salario_hora = 0.0);
+
+    virtual ~Funcionario();
+
+    void set_nome_usuario(string nome_usuario);
+    string get_nome_usuario() const;
+
+    void set_senha(string senha);
+    string get_senha() const;
+
+    void set_tipo(string tipo);
+    string get_tipo() const;
+
+    void set_funcao(string funcao);
+    string get_funcao() const;
+
+    void set_salario_hora(double salario_hora);
+    double get_salario_hora() const;
+
+    double calcular_horas_semana();
+    double calcular_horas_mes();
+
+    virtual double calcular_salario();
+    virtual void mostrar_salario();
+    virtual void mostrar_vendas();
+
+    friend ostream &operator<<(ostream, const Funcionario &);
+  };
--- src/classes/pessoa.cpp
+++ src/classes/pessoa.cpp
@@ -2,21 +2,23 @@
 
 using namespace std;
 
-Pessoa::Pessoa(string nome) :
-  nome(nome) {}
+Pessoa::Pessoa(string nome)
+{
+  this->nome = nome;
+}
 
-  Pessoa::~Pessoa() {}
+string Pessoa::getNome() const
+{
+  return this->nome;
+}
 
-  void Pessoa::setNome(string nome){
-    this->nome = nome;
-  }
+void Pessoa::setNome(string nome)
+{
+  this->nome = nome;
+}
 
-  string Pessoa::getNome() const {
-    return this->nome;
-  }
-
-  ostream &operator <<(ostream &out, const Pessoa &obj) {
-    out << "\x1b[1m\x1b[34mNome:\x1b[0m " << obj.getNome();
-    return out;
-  }
-
+ostream &operator<<(ostream &out, const Pessoa &obj)
+{
+  out << "\x1b[1m\x1b[34mNome:\x1b[0m " << obj.getNome();
+  return out;
+}
