d0ab5837f6268b5c73575b05698c472c7fc8e938
Caio Lucas <lienscarlet@gmail.com>
2023-06-29 09:51:37 -0300

    add some corrections

--- src/classes/vendedor.cpp
+++ src/classes/vendedor.cpp
@@ -1,14 +1,15 @@
 #include "vendedor.h"
+#include "../utils/consts.h"
 
 Vendedor::Vendedor(string nome, string nome_usuario, string senha, string funcao, double salario)
     : Funcionario(nome, nome_usuario, senha, funcao, salario) {}
 
-vector<Venda> Vendedor::getVendas() const
+vector<Venda *> Vendedor::getVendas() const
 {
   return this->vendas;
 }
 
-void Vendedor::addVenda(Venda venda)
+void Vendedor::addVenda(Venda *venda)
 {
   this->vendas.push_back(venda);
 }
@@ -17,13 +18,13 @@ double Vendedor::getValorVendas(Data data) const
 {
   double valor = 0.0;
 
-  for (Venda v : this->vendas)
+  for (Venda *v : this->vendas)
   {
-    int mesVenda = v.getData().getMes();
-    int anoVenda = v.getData().getAno();
+    int mesVenda = v->getData().getMes();
+    int anoVenda = v->getData().getAno();
 
     if (data.getMes() == mesVenda && data.getAno() == anoVenda)
-      valor += v.getValor();
+      valor += v->getValor();
   }
 
   return valor;
@@ -31,9 +32,26 @@ double Vendedor::getValorVendas(Data data) const
 
 double Vendedor::getSalarioMes(Data data) const
 {
-  double salario = 0.0;
-  salario = this->getSalarioHora() + this->getHorasSemana(data);
-  return salario;
+  int mes = data.getMes();
+  int ano = data.getAno();
+  double horas = 0.0;
+
+  for (Ponto p : this->getPontos())
+  {
+    int mesPonto = p.getData().getMes();
+    int anoPonto = p.getData().getAno();
+
+    if (mes == mesPonto && ano == anoPonto)
+    {
+      double horasDia = p.getHorasTrabalhadas();
+
+      if (horasDia > MAX_HORAS_NORMAIS)
+        horas += MAX_HORAS_NORMAIS + (horasDia - MAX_HORAS_NORMAIS) * HORA_EXTRA_VENDEDOR;
+      else
+        horas += horasDia;
+    }
+  }
+  return this->getSalarioHora() * horas;
 }
 
 double Vendedor::getBonificacao(Data data) const
@@ -52,14 +70,11 @@ double Vendedor::getSalarioFinal(Data data) const
 
 ostream &operator<<(ostream &out, const Vendedor &obj)
 {
-  out << "\x1b[1mNome:\x1b[0m" << obj.getNome() << endl
-      << "\t"
-      << "\x1b[1mUsuário:\x1b[0m " << obj.getNomeUsuario() << endl
-      << "\t"
-      << "\x1b[1mFunção:\x1b[0m " << obj.getFuncao()
-      << "\t"
-      << "\x1b[1mTipo:\x1b[0m Vendedor"
-      << "\x1b[1mSalário por hora:\x1b[0m " << obj.getSalarioHora();
+  out << "\t\x1b[1mNome:\x1b[0m" << obj.getNome() << endl
+      << "\t\x1b[1mUsuário:\x1b[0m " << obj.getNomeUsuario() << endl
+      << "\t\x1b[1mFunção:\x1b[0m " << obj.getFuncao()
+      << "\t\x1b[1mTipo:\x1b[0m Vendedor"
+      << "\t\x1b[1mSalário por hora:\x1b[0m " << obj.getSalarioHora();
 
   return out;
 }
